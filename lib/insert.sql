1d7ece5ccd7af96997b3d5842712d1109a49b948
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-26 15:16:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name , age, gender, color temperment, alive) VALUES 
+INSERT INTO bears (name, age, gender, color temperment, alive) VALUES 
 (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
 (2,"Rowdy", 10, "F", "Brown", "intense", 1),
 (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
