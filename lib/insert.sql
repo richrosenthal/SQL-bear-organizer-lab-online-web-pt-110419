e30a9025a551f300d83cc47ef64504bfd6d4bbbb
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-26 15:17:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO bears (name, age, gender, color temperment, alive) VALUES 
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "F", "Brown", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+("Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+("Rowdy", 10, "F", "Brown", "intense", 1),
+("Tabitha", 6, "F", "dark brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+("Melissa", 13, "F", "dark brown", "goofy", 1),
+("Grinch", 2, "M", "Black", "Grinchy", 1),
+("Wendy", 6, "F", "Blue", "naive", 1),
+(null, 20, "M", "black", "aggressive", 0);
 
 
 
